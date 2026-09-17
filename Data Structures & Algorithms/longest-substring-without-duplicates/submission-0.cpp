06d38ad8f03a415c786776f0328096e3a51f1db9
Serena Flaxenburg <sgflax@umich.edu>
2026-09-16 21:52:21 -0400

    Add: longest-substring-without-duplicates - submission-0

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-0.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        if(s.size() == 0) return 0;
+        if(s.size() == 1) return 1;
+
+        unordered_set<int> mySet;
+        int l = 0;
+        int res = 0;
+        
+        for(int r = 0; r < s.size(); ++r){
+            while(mySet.find(s[r]) != mySet.end()){
+                mySet.erase(s[l]);
+                l++;
+            }
+            mySet.insert(s[r]);
+            res = max(res, r - l + 1);
+        }
+        return res;
+    }
+};
